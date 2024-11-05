3da669b8864252f712727bd80a1fe680e29978a8
Shreyas <shreyasnarule@gmail.com>
2024-11-05 18:44:33 +0530

    App_ID and APP_SIGN Preserved

--- app/src/main/java/com/example/streamingapp/utils/AppConst.kt
+++ app/src/main/java/com/example/streamingapp/utils/AppConst.kt
@@ -1,4 +1,4 @@
 package com.example.streamingapp.utils
 
-const val APP_ID : Long= 1667796106
-const val APP_SIGN : String = "e10de51cf5d3e735ff9a970d6537472f11abb924bffec2914172720dac0a502d"
+const val APP_ID : Long= 21546L
+const val APP_SIGN : String = "Get your Own Key from ZEGOCLOUD"
